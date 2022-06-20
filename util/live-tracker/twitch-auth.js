af0a23322dfba8f1a795ee10b3c81d1d8c4f683f
Kenneth Verheggen <javasaurusdev@gmail.com>
2022-06-20 11:23:38 +0200

    Finishing oauth implicit workflow for testing

--- util/live-tracker/twitch-auth.js
+++ util/live-tracker/twitch-auth.js
@@ -1,22 +1,22 @@
-function doAuth() {
+var client_id = "wep38t0x9n5oe1iyid2ff21o5iesjp";
+var redirect = "https://javasaurusstudios.github.io/viewers/util/live-tracker/twitch-check.html";
+var access_token="";
 
-    var client_id = document.getElementById("clientID").value;
-    var redirect = "https://javasaurusstudios.github.io/viewers/util/live-tracker/twitch-check.html";
-
-    alert(client_id);
-    alert(redirect);
-
+function requestAuth() {
     location.href = 'https://id.twitch.tv/oauth2/authorize?client_id=' + client_id + '&redirect_uri=' + encodeURIComponent(redirect) + '&response_type=token';
     if (document.location.hash && document.location.hash != '') {
         var parsedHash = new URLSearchParams(window.location.hash.slice(1));
-        if (parsedHash.get('access_token')) {
-            var access_token = parsedHash.get('access_token');
-            document.getElementById('access_token').textContent = 'Your Access Key from the #url: ' + access_token;
-        } else if (document.location.search && document.location.search != '') {
+        if (!parsedHash.get('access_token') && (document.location.search && document.location.search != '')) {
             var parsedParams = new URLSearchParams(window.location.search);
             if (parsedParams.get('error_description')) {
                 document.getElementById('access_token').textContent = parsedParams.get('error') + ' - ' + parsedParams.get('error_description');
             }
         }
     }
+}
+
+function loadAuth() {
+    const urlParams = new URLSearchParams(window.location.search);
+    access_token = urlParams.get('access_token')
+    document.getElementById("access_token").textContent = access_token;
 }
--- util/live-tracker/twitch-check.js
+++
@@ -1,7 +0,0 @@
-function loadAuth() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const access_token = urlParams.get('access_token')
-    console.log(access_token);
-    alert(access_token);
-    document.getElementById("access_token").value = access_token;
-}
