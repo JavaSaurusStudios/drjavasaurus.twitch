9fce7396b12d8646d8edb1adcb17619004391c22
Kenneth Verheggen <javasaurusdev@gmail.com>
2022-06-20 11:00:49 +0200

    Check for token

---
+++ util/live-tracker/twitch-check.js
@@ -0,0 +1,6 @@
+function loadAuth() {
+    const access_token = urlParams.get('access_token')
+    console.log(access_token);
+    alert(access_token);
+    document.getElementById("access_token").value = access_token;
+}
