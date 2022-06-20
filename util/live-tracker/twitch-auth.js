5b06aa58caf7f0b48e2af5927cebe301ff32c8f7
Kenneth Verheggen <javasaurusdev@gmail.com>
2022-06-20 11:33:55 +0200

    Fix URL parameter

--- util/live-tracker/twitch-auth.js
+++ util/live-tracker/twitch-auth.js
@@ -1,22 +1,21 @@
 var client_id = "wep38t0x9n5oe1iyid2ff21o5iesjp";
 var redirect = "https://javasaurusstudios.github.io/viewers/util/live-tracker/twitch-check.html";
-var access_token="";
+var access_token = "";
 
 function requestAuth() {
     location.href = 'https://id.twitch.tv/oauth2/authorize?client_id=' + client_id + '&redirect_uri=' + encodeURIComponent(redirect) + '&response_type=token';
-    if (document.location.hash && document.location.hash != '') {
-        var parsedHash = new URLSearchParams(window.location.hash.slice(1));
-        if (!parsedHash.get('access_token') && (document.location.search && document.location.search != '')) {
-            var parsedParams = new URLSearchParams(window.location.search);
-            if (parsedParams.get('error_description')) {
-                document.getElementById('access_token').textContent = parsedParams.get('error') + ' - ' + parsedParams.get('error_description');
-            }
-        }
-    }
 }
 
 function loadAuth() {
-    const urlParams = new URLSearchParams(window.location.search);
-    access_token = urlParams.get('access_token')
+    access_token = getParameterByName('access_token');
     document.getElementById("access_token").textContent = access_token;
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?#&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
